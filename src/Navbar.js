1f718e5087fd62443c4428c71cd928d97704e753
Mike <mjmailliard@gmail.com>
2019-01-08 13:58:55 -0700

    initial commit with name tag animation and navbar

---
+++ src/Navbar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class Navbar extends Component {
+    render() {
+      return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">Mike Mailliard</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarColor01">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Projects</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact Me</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">About</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+      )
+    }
+}
