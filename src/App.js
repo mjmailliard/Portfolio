1b838ef4232433c73b5c621cf0974850b8ad46be
Mike <mjmailliard@gmail.com>
2019-01-10 21:34:24 -0700

    moved Nametag to a component

--- src/App.js
+++ src/App.js
@@ -3,27 +3,18 @@ import './App.css';
 // import Navbar from './Components/Navbar'
 import Layout from './Components/Layout';
 // import Layout from './Components/Layout'
+import NameTag from './Components/NameTag'
 import Routing from './Routing'
 class App extends Component {
   render() {
     return (
       <div className="">
-      <Layout>
+        <Layout>
+
+          <NameTag/>
 
 
-
-<div className='nameTag'>
-<div name="maintag" className="helloMyNameIs">
-   <section className="hello">Hello</section> 
-   <section className="mynameis">my name is</section>
-  </div>
-  <div name="namespace" className="name">
-    Mike
-  </div>
-  <div name="bottomBar" className="bottomBar">
-  </div>
-</div>
-</Layout>
+        </Layout>
       </div>
     );
   }
---
+++ src/Components/NameTag.js
@@ -0,0 +1,23 @@
+import React, { Component, Fragment } from 'react';
+
+
+
+export default class NameTag extends Component {
+    render() {
+      return (
+          <Fragment>
+            <div className='nameTag'>
+                <div name="maintag" className="helloMyNameIs">
+                    <section className="hello">Hello</section> 
+                    <section className="mynameis">my name is</section>
+                </div>
+                <div name="namespace" className="name">
+                    Mike
+                </div>
+                <div name="bottomBar" className="bottomBar">
+                </div>
+            </div>
+         </Fragment>
+      )
+    }
+}
